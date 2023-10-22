ec6415850febc3fffdd76178aec4874e975e7bf2
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2023-10-22 09:30:14 +0200

    implemented the first part of the new logic for arg expanding, (word counting), doesnt quite work yet but we'll get there

--- src/read_sequences.c
+++ src/read_sequences.c
@@ -6,69 +6,104 @@
 /*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 20:41:15 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/10/22 00:35:30 by arthur           ###   ########.fr       */
+/*   Updated: 2023/10/22 09:27:00 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-char *replace(char *current, size_t *i)
-{
-    char *temp;
-    char *new;
-    char *expanded_content;
+// char *replace(char *current, size_t *i)
+// {
+//     char *temp;
+//     char *new;
+//     char *expanded_content;
 	
-	expanded_content = "EXPANSION CONTENT";
-    size_t index;
-	index = *i;
-    temp = ft_strndup(current, index);
-    if (!temp)
-        return NULL;
-    while (current[index] && !myisspace(current[index]))
-        index++;
-    *i = index;
-    new = ft_strjoin(temp, expanded_content);
-    if (!new)
-		return (free(temp), NULL);
-	free(temp);
-    return new;
-}
+// 	expanded_content = "EXPANSION CONTENT";
+//     size_t index;
+// 	index = *i;
+//     temp = ft_strndup(current, index);
+//     if (!temp)
+//         return NULL;
+//     while (current[index] && !myisspace(current[index]))
+//         index++;
+//     *i = index;
+//     new = ft_strjoin(temp, expanded_content);
+//     if (!new)
+// 		return (free(temp), NULL);
+// 	free(temp);
+//     return new;
+// }
 
-char *expand_dquotes(t_quotes *current)
+// char *expand_dquotes(t_quotes *current)
+// {
+//     size_t i = 0;
+//     char *new;
+//     char *temp;
+
+// 	new = NULL;
+//     while (current->sequence[i])
+//     {
+//         if (current->sequence[i] == '$')
+//         {
+//             temp = replace(current->sequence, &i);
+//             if (!temp)
+//                 return NULL;
+//             if (!new)
+//                 new = temp;
+//             else
+//             {
+//                 char *old_new = new;
+//                 new = ft_strjoin(old_new, temp);
+// 				if (!new)
+// 					return (free(old_new), free(temp), NULL);
+// 				printf("new after joining: %s\n i : %zu\n", new, i);
+
+//                 free(old_new);
+//                 free(temp);
+//             }
+//         }
+//         else
+//             i++;
+//     }
+//     if (!new)
+//         return ft_strdup(current->sequence);
+//     free(current->sequence);
+//     return new;
+// }
+
+int count_words(char *seq)
 {
-    size_t i = 0;
-    char *new;
-    char *temp;
+    int i;
+    int ret;
 
-	new = NULL;
-    while (current->sequence[i])
+    i = 0;
+    ret = 0;
+    while (seq[i])
     {
-        if (current->sequence[i] == '$')
+        if (seq[i] == '$')
         {
-            temp = replace(current->sequence, &i);
-            if (!temp)
-                return NULL;
-            if (!new)
-                new = temp;
-            else
-            {
-                char *old_new = new;
-                new = ft_strjoin(old_new, temp);
-				if (!new)
-					return (free(old_new), free(temp), NULL);
-				printf("new after joining: %s\n i : %zu\n", new, i);
-
-                free(old_new);
-                free(temp);
-            }
+            ret++;
+            while (seq[i] && seq[i] != '$' && !myisspace(seq[i]))
+                i++;
         }
         else
-            i++;
+        {
+            ret++;
+            while (seq[i] && seq[i] != '$')
+                i++;
+        }
+        i++;
     }
-    if (!new)
-        return ft_strdup(current->sequence);
-    free(current->sequence);
-    return new;
+    return (ret);  
+}
+
+char    *expand_dquotes(char *sequence)
+{
+    int arr_size;
+    
+    arr_size = count_words(sequence);
+    printf("word count: %d\n", arr_size);
+    return (NULL);
 }
 
 /*TODO: idea for different expanding approach, see github issues*/
@@ -80,8 +115,7 @@ void	expand_sequences(t_shell *data)
 	while (head)
 	{
 		if (head->status == IN_DOUBLE_QUOTES)
-			head->sequence = expand_dquotes(head);
-		printf("%s\n", head->sequence);
+			head->sequence = expand_dquotes(head->sequence);
 		head = head->next;
 	}
 }
