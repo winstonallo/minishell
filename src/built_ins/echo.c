250cadc6ab3eec2f240dcb62893c65abbd2f0c8c
winstonallo <arthurbiedchar@gmail.com>
2023-11-02 17:45:22 +0100

    implemented echo with -n flag

--- src/built_ins/echo.c
+++ src/built_ins/echo.c
@@ -6,7 +6,7 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:38:37 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/11/02 16:52:37 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/11/02 17:44:28 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,15 @@ int	echo(t_shell *data, int newline)
 	int	i;
 
 	i = 0;
-	if (ft_strncmp((*data->cmd_table)->args[1], "-n", 3) == 0)
+	if ((*data->cmd_table)->args[1]
+		&& ft_strncmp((*data->cmd_table)->args[1], "-n", 3) == 0)
+	{
 		newline = 1;
+		i++;
+	}
 	while ((*data->cmd_table)->args[++i])
 		printf("%s", (*data->cmd_table)->args[i]);
+	if (!newline)
+		printf("\n");
+	return (0);
 }
