5adef0d8bc1aaa7af68246a2dfa7d918219fa363
sa4-E <safiye.anna@icloud.com>
2023-11-30 00:04:05 +0100

    echo does not work but like the logic

--- src/built_ins/echo.c
+++ src/built_ins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sstanfel <sstanfel@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:38:37 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/11/29 17:01:14 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/11/29 23:58:42 by sstanfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,62 @@
  * The function "echo" prints out the sequences stored in a linked list,
  * with the option to omit a newline character at the end.
  */
-int	echo(t_cmd_table *head, int newline, t_shell *data)
+int	mooore_ns(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (!head->args[1])
-		return (printf("\n"), SUCCESS);
-	if (ft_strncmp(head->args[1], "-n", 3) == 0)
-	{
-		newline = 0;
+	while (str[i] && str[i] == 'n')
 		i++;
-	}
-	while (head->args[++i])
+	if (i != (int)ft_strlen(str))
+		return (1);
+	return (0);
+}
+int	look_for_n(char **arg)
+{
+	int	i;
+
+	i = 1;
+	while (arg[i])
 	{
-		printf("%s", head->args[i]);
-		if (head->args[i + 1])
-			printf(" ");
+		if (arg[i][0] != '-' || arg[i][1] != 'n' || mooore_ns(arg[i]) + 1)
+		{
+			printf("here\n");
+			break ;
+		}
+		else
+			i++;
+	}
+	return (i);
+}
+int	echo(t_cmd_table *head, int newline, t_shell *data)
+{
+	int	i;
+
+	i = look_for_n(head->args);
+	//printf("i: %d\n", i);
+	if (!head->args[1])
+		return (SUCCESS);
+	if (i == 1)
+	{
+		while (head->args[i])
+		{
+			printf("%s", head->args[i]);
+			if (head->args[i + 1])
+				printf(" ");
+			i++;
+		}
+		newline = 1;
+	}
+	else
+	{
+		while (head->args[i])
+		{
+			printf("%s", head->args[i]);
+			if (head->args[i + 1])
+				printf(" ");
+			i++;
+		}
 	}
 	if (newline)
 		printf("\n");
