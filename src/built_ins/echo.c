fef63dab1a108a83c94db42635da9b7797a6028a
sa4-E <safiye.anna@icloud.com>
2023-11-30 03:52:59 +0100

    added -nnnnnnnnnnnnn for echo

--- src/built_ins/echo.c
+++ src/built_ins/echo.c
@@ -3,15 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sstanfel <sstanfel@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:38:37 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/11/27 15:01:02 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/11/30 03:50:35 by sstanfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+int	check_n(char *arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[++i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+	}
+	return (1);
+}
 /**
  * The function "echo" prints out the sequences stored in a linked list,
  * with the option to omit a newline character at the end.
@@ -23,10 +35,13 @@ int	echo(t_cmd_table *head, int newline)
 	i = 0;
 	if (!head->args[1])
 		return (printf("\n"), SUCCESS);
-	if (ft_strncmp(head->args[1], "-n", 3) == 0)
+	if (ft_strncmp(head->args[1], "-n", 2) == 0)
 	{
-		newline = 0;
-		i++;
+		if (check_n(head->args[1]))
+		{
+			newline = 0;
+			i++;
+		}
 	}
 	while (head->args[++i])
 	{
